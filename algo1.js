ca5b58a277f3eb8169375aff8a138380523b2bfe
dylan belouis <dylan.belouis@gmail.com>
2019-10-11 12:11:35 +0200

    algo1 et CSS avancement

--- algo1.js
+++ algo1.js
@@ -17,3 +17,37 @@ Au final la liste devra ressembler à :
 
 Écrire une fonction qui retourne un tableau comprenant tous les numéros des sièges.
 */
+
+
+let newb = 1
+/*const calcul = function (i,b) {
+  let i = 1;
+let b = 1;
+  do { 
+    calcul(i,b);
+  } while (i != 26 && b != 100);  
+  do {
+    b = b++;
+    result = [i, b];
+  } while (b < 100);
+  function second (i,b) {
+    if ( b === 100) {
+      let b = 0;
+      let i = i + 1;
+      return result;
+    }
+   
+  }
+}*/
+
+let i = 1;
+let a = 1;
+let result = [];
+for ( let i = 1; i < 100; i++) {
+  result.push(i,a)
+for ( let a = 1; i > 100; a + 1){
+  result.push(i,a)
+  return result
+}
+}
+console.log(result)
